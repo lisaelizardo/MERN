2a75b653faa15a4e202031e26363311e1facabce
Deneene Whitaker <lisaelizrdo@gmail.com>
2023-03-29 21:53:48 -0500

    add

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+  <ApolloProvider client={client}>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route 
+            path='/' 
+            element={<SearchBooks />} 
+          />
+          <Route 
+            path='/saved' 
+            element={<SavedBooks />} 
+          />
+          <Route 
+            path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
+          />
+        </Routes>
+      </>
+    </Router>
+  </ApolloProvider>
+
+  );
+}
+
+export default App;
---
+++ client/src/graphql/mutations.js
@@ -0,0 +1,41 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation loginUser($email: String!, $password: String!) {
+      login(email: $email, password: $password) {
+        email
+        password
+      }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!){
+      addUser(username: $username, email: $email, password: $password){
+        username
+        email
+        password
+      }
+    }
+`;
+
+// export const SAVE_BOOK = gql`
+//     mutation saveBook($input: {
+//       bookId
+//       authors
+//       description
+//       title
+//       image
+//       link
+//     }) {
+
+//     }
+// `;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+      removeBook(bookId: $bookId) {
+        id
+      }
+    }
+`;
